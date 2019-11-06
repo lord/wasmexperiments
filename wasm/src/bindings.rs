727a2d9573fcbca8e758a3ea237da4b51620d03c
Robert Lord <robert@lord.io>
2019-11-06 17:15:54 -0500

    basic host call schema

--- wasm/src/bindings.rs
+++ wasm/src/bindings.rs
@@ -1,8 +1,29 @@
+type Handle = usize;
+
 extern "C" {
-    pub fn kx_sleep();
-    pub fn kx_channel_create(handle_a: *mut usize, handle_b: *mut usize);
-    pub fn kx_channel_read(handle_a: *mut usize, handle_b: *mut usize);
-    pub fn kx_debug_msg(msg: i32);
+    pub fn kp_channel_create(handle_a: *mut Handle, handle_b: *mut Handle);
+    pub fn kp_channel_write(channel: Handle,
+                            bytes: *mut u8,
+                            handles: *mut Handle,
+                            byte_count: usize,
+                            handle_count: usize);
+    pub fn kp_channel_read(channel: Handle,
+                            bytes: *mut u8,
+                            handles: *mut Handle,
+                            byte_count: usize,
+                            handle_count: usize,
+                            byte_actual_count: *mut usize,
+                            handle_actual_count: *mut usize);
+
+    pub fn kp_pollgroup_create(handle: *mut Handle);
+    pub fn kp_pollgroup_insert(pollgroup: Handle, channel: Handle, token: u32);
+    pub fn kp_pollgroup_wait(handle: Handle, token: *mut u32);
+    pub fn kp_pollgroup_cancel(handle: Handle, token: u32);
+
+    pub fn kp_generic_close(handle: Handle);
+
+    pub fn kp_sleep(time: u32);
+    pub fn kp_debug_msg(msg: i32);
 }
 
 #[no_mangle]
--- wasm/src/main.rs
+++ wasm/src/main.rs
@@ -3,8 +3,8 @@ mod bindings;
 fn main() {
     let mut n: i32 = 1;
     loop {
-        unsafe { bindings::kx_debug_msg(n); }
-        unsafe { bindings::kx_sleep() }
+        unsafe { bindings::kp_debug_msg(n); }
+        unsafe { bindings::kp_sleep(500000) }
         n += 1;
     }
 }
