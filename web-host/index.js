a849b96a18e0a96fa137d90e7e81e4833279cf1c
Robert Lord <robert@lord.io>
2019-11-06 23:49:13 -0500

    fill out rest of fns

--- web-host/index.js
+++ web-host/index.js
@@ -28,6 +28,30 @@ class Instance {
       this.instance.exports.main();
     })
   }
+  kp_channel_create() {
+    console.error("call to unimplemented function kp_channel_create")
+  }
+  kp_channel_write() {
+    console.error("call to unimplemented function kp_channel_write")
+  }
+  kp_channel_read() {
+    console.error("call to unimplemented function kp_channel_read")
+  }
+  kp_pollgroup_create() {
+    console.error("call to unimplemented function kp_pollgroup_create")
+  }
+  kp_pollgroup_insert() {
+    console.error("call to unimplemented function kp_pollgroup_insert")
+  }
+  kp_pollgroup_wait() {
+    console.error("call to unimplemented function kp_pollgroup_wait")
+  }
+  kp_pollgroup_cancel() {
+    console.error("call to unimplemented function kp_pollgroup_cancel")
+  }
+  kp_generic_close() {
+    console.error("call to unimplemented function kp_generic_close")
+  }
   kp_sleep(ns) {
     this.wrap_async(() => {
       return new Promise(resolve => setTimeout(resolve, ns / 1000))
@@ -39,8 +63,16 @@ class Instance {
   async run_main() {
     let env = {};
     [
-      "kp_debug_msg",
+      "kp_channel_create",
+      "kp_channel_write",
+      "kp_channel_read",
+      "kp_pollgroup_create",
+      "kp_pollgroup_insert",
+      "kp_pollgroup_wait",
+      "kp_pollgroup_cancel",
+      "kp_generic_close",
       "kp_sleep",
+      "kp_debug_msg",
     ].forEach(fn => {env[fn] = (...args) => this[fn](...args)});
     let results = await WebAssembly.instantiate(this.wasmBytes, {env});
     this.instance = results.instance;
