c0d47d0a2004a67364a249e2036394d7b9b0b6c3
Robert Lord <robert@lord.io>
2019-11-13 22:56:17 -0500

    standard --fix

--- web-host/index.js
+++ web-host/index.js
@@ -1,122 +1,122 @@
 class Dispatch {
-  constructor() {
+  constructor () {
     // maps process ids to WasmProcess instances
     this.processes = {}
     // maps global channel ids -> {pid, handle}
     this.channels = {}
-    this.nextId = 1;
+    this.nextId = 1
   }
 
-  newProcess(wasmFile) {
-    let id = this.nextId;
-    this.nextId += 1;
+  newProcess (wasmFile) {
+    const id = this.nextId
+    this.nextId += 1
 
-    this.processes[id] = new WasmProcess("out.wasm", id, this);
+    this.processes[id] = new WasmProcess('out.wasm', id, this)
 
-    return id;
+    return id
   }
 
-  newChannel(pid, handleA, handleB) {
-    let global_a = this.nextId;
-    this.nextId += 1;
-    let global_b = this.nextId;
-    this.nextId += 1;
+  newChannel (pid, handleA, handleB) {
+    const global_a = this.nextId
+    this.nextId += 1
+    const global_b = this.nextId
+    this.nextId += 1
 
-    this.processes[pid].channels[handleA] = global_a;
-    this.processes[pid].channels[handleB] = global_b;
+    this.processes[pid].channels[handleA] = global_a
+    this.processes[pid].channels[handleB] = global_b
 
-    this.channels[global_a] = {pid, handle: handleA, queue: [], pair: global_b};
-    this.channels[global_b] = {pid, handle: handleB, queue: [], pair: global_a};
+    this.channels[global_a] = { pid, handle: handleA, queue: [], pair: global_b }
+    this.channels[global_b] = { pid, handle: handleB, queue: [], pair: global_a }
   }
 
-  closeChannel(globalId) {
-    let channel = this.channels[globalId];
-    this.channels[globalId] = false;
-    this.processes[channel.pid].channels[channel.handle] = false;
+  closeChannel (globalId) {
+    const channel = this.channels[globalId]
+    this.channels[globalId] = false
+    this.processes[channel.pid].channels[channel.handle] = false
 
     if (this.channels[channel.pair]) {
       // set this pair's pair attribute to zero to indicate channel is closed
-      this.channels[channel.pair].pair = 0;
+      this.channels[channel.pair].pair = 0
     }
   }
 }
 
 class WasmProcess {
-  constructor(wasmFile, pid, dispatch){
-    this.dispatch = dispatch;
-    this.pid = pid;
-    this.worker = new Worker("/thread.js");
+  constructor (wasmFile, pid, dispatch) {
+    this.dispatch = dispatch
+    this.pid = pid
+    this.worker = new Worker('/thread.js')
     /// maps internal channel ids to global ids in dispatch
-    this.channels = {};
+    this.channels = {}
     /// maps ring ids -> ptr structs
-    this.rings = {};
+    this.rings = {}
     this.memory = new WebAssembly.Memory({
       initial: 20,
       maximum: 10000,
-      shared: true,
-    });
-    this.worker.postMessage({wasmFile, memory: this.memory});
+      shared: true
+    })
+    this.worker.postMessage({ wasmFile, memory: this.memory })
     this.worker.onmessage = (e) => {
-      this.handleMsg(e.data);
-    };
+      this.handleMsg(e.data)
+    }
   }
 
   handleMsg (data) {
-    if (data.msg === "kp_channel_create") {
+    if (data.msg === 'kp_channel_create') {
       this.kp_channel_create(data)
-    } else if (data.msg === "kp_ring_create") {
+    } else if (data.msg === 'kp_ring_create') {
       this.kp_ring_create(data)
-    } else if (data.msg === "kp_ring_enter") {
+    } else if (data.msg === 'kp_ring_enter') {
       this.kp_ring_enter(data)
-    } else if (data.msg === "kp_generic_close") {
+    } else if (data.msg === 'kp_generic_close') {
       this.kp_generic_close(data)
     } else {
-      console.error("unknown msg:", data)
+      console.error('unknown msg:', data)
     }
   }
 
-  kp_channel_create({a_id, b_id}) {
+  kp_channel_create ({ a_id, b_id }) {
     // TODO channel create message could come AFTER I/O is enqueued.
-    this.dispatch.newChannel(this.pid, a_id, b_id);
+    this.dispatch.newChannel(this.pid, a_id, b_id)
   }
 
-  kp_ring_create({ring_id, ptrs}) {
-    this.rings[ring_id] = ptrs;
-    this.rings[ring_id].polling = false;
+  kp_ring_create ({ ring_id, ptrs }) {
+    this.rings[ring_id] = ptrs
+    this.rings[ring_id].polling = false
   }
 
-  kp_ring_enter({ring_id}) {
+  kp_ring_enter ({ ring_id }) {
     if (!this.rings[ring_id].polling) {
-      this.rings[ring_id].polling = true;
-      let view = (new Int32Array(this.memory.buffer));
-      Atomics.store(view, this.rings[ring_id].flags >> 2, 0b010);
-      setTimeout(() => this.pollRing(ring_id), 1);
+      this.rings[ring_id].polling = true
+      const view = (new Int32Array(this.memory.buffer))
+      Atomics.store(view, this.rings[ring_id].flags >> 2, 0b010)
+      setTimeout(() => this.pollRing(ring_id), 1)
     }
-    this.checkRing();
+    this.checkRing()
   }
 
-  kp_generic_close({handle}) {
+  kp_generic_close ({ handle }) {
     if (this.rings[handle]) {
-      this.rings[handle] = false;
+      this.rings[handle] = false
     } else if (this.channels[handle]) {
       this.dispatch.closeChannel(this.channels[handle])
     } else {
-      console.error("attempted to close unknown handle:", handle);
+      console.error('attempted to close unknown handle:', handle)
     }
   }
 
-  pollRing(ringId) {
+  pollRing (ringId) {
     // TODO actually maybe poll several times?
-    this.rings[ringId].polling = false;
-    let view = (new Int32Array(this.memory.buffer));
-    Atomics.store(view, this.rings[ringId].flags >> 2, 0b000);
+    this.rings[ringId].polling = false
+    const view = (new Int32Array(this.memory.buffer))
+    Atomics.store(view, this.rings[ringId].flags >> 2, 0b000)
     this.checkRing(ringId)
   }
 
-  checkRing(ringId) {
-    console.warn("checking for new messages unimplemented")
+  checkRing (ringId) {
+    console.warn('checking for new messages unimplemented')
   }
 }
 
-let dispatch = new Dispatch()
-dispatch.newProcess("out.wasm")
+const dispatch = new Dispatch()
+dispatch.newProcess('out.wasm')
--- web-host/thread.js
+++ web-host/thread.js
@@ -1,51 +1,51 @@
 class Instance {
-  constructor(bytes) {
-    this.wasmBytes = bytes;
-    this.instance = null;
-    this.memoryView = null;
-    this.memoryViewU32 = null;
-    this.memoryViewI32 = null;
+  constructor (bytes) {
+    this.wasmBytes = bytes
+    this.instance = null
+    this.memoryView = null
+    this.memoryViewU32 = null
+    this.memoryViewI32 = null
 
-    this.rings = {};
-    this.channels = {};
-    this.nextHandleId = 1;
+    this.rings = {}
+    this.channels = {}
+    this.nextHandleId = 1
 
-    this.rewindBufferPtr = null;
-    this.rewindActive = false;
-    this.rewindReturnValue = null;
+    this.rewindBufferPtr = null
+    this.rewindActive = false
+    this.rewindReturnValue = null
   }
 
-  getUint32(ptr) {
-    let n = 0;
-    n += this.memoryView[ptr+3];
-    n = n << 8;
-    n += this.memoryView[ptr+2];
-    n = n << 8;
-    n += this.memoryView[ptr+1];
-    n = n << 8;
-    n += this.memoryView[ptr+0];
-    return n;
+  getUint32 (ptr) {
+    let n = 0
+    n += this.memoryView[ptr + 3]
+    n = n << 8
+    n += this.memoryView[ptr + 2]
+    n = n << 8
+    n += this.memoryView[ptr + 1]
+    n = n << 8
+    n += this.memoryView[ptr + 0]
+    return n
   }
 
-  wait(ptr, val, timeout) {
-    let idx = ptr >> 2;
+  wait (ptr, val, timeout) {
+    const idx = ptr >> 2
     if (val >= Math.pow(2, 31)) {
-        val = val - Math.pow(2, 32)
+      val = val - Math.pow(2, 32)
     }
-    return Atomics.wait(this.memoryViewI32, idx, val, timeout);
+    return Atomics.wait(this.memoryViewI32, idx, val, timeout)
   }
 
-  setUint32(ptr, value) {
-    this.memoryView[ptr+3] = (0xFF000000 & value) >> (8*3);
-    this.memoryView[ptr+2] = (0x00FF0000 & value) >> (8*2);
-    this.memoryView[ptr+1] = (0x0000FF00 & value) >> 8;
-    this.memoryView[ptr+0] = (0x000000FF & value);
+  setUint32 (ptr, value) {
+    this.memoryView[ptr + 3] = (0xFF000000 & value) >> (8 * 3)
+    this.memoryView[ptr + 2] = (0x00FF0000 & value) >> (8 * 2)
+    this.memoryView[ptr + 1] = (0x0000FF00 & value) >> 8
+    this.memoryView[ptr + 0] = (0x000000FF & value)
   }
 
-  wrap_async(async_fn) {
+  wrap_async (async_fn) {
     if (this.rewindActive) {
-      this.rewindActive = false;
-      this.instance.exports.asyncify_stop_rewind();
+      this.rewindActive = false
+      this.instance.exports.asyncify_stop_rewind()
       return this.rewindReturnValue
     }
     // pointer to start of temp buffer
@@ -53,200 +53,200 @@ class Instance {
     // pointer to end of temp buffer
     this.setUint32(this.rewindBufferPtr + 4, this.rewindBufferPtr + 8 + 1024)
 
-    this.instance.exports.asyncify_start_unwind(this.rewindBufferPtr);
+    this.instance.exports.asyncify_start_unwind(this.rewindBufferPtr)
 
-    let done = (res) => {
-      this.rewindReturnValue = res;
-      this.rewindActive = true;
-      this.instance.exports.asyncify_start_rewind(this.rewindBufferPtr);
-      this.instance.exports.main();
-      this.instance.exports.asyncify_stop_unwind();
+    const done = (res) => {
+      this.rewindReturnValue = res
+      this.rewindActive = true
+      this.instance.exports.asyncify_start_rewind(this.rewindBufferPtr)
+      this.instance.exports.main()
+      this.instance.exports.asyncify_stop_unwind()
     }
     async_fn(done)
   }
 
-  kp_channel_create(handle_a_ptr, handle_b_ptr) {
-    const idA = this.nextHandleId;
-    this.nextHandleId += 1;
-    const idB = this.nextHandleId;
-    this.nextHandleId += 1;
+  kp_channel_create (handle_a_ptr, handle_b_ptr) {
+    const idA = this.nextHandleId
+    this.nextHandleId += 1
+    const idB = this.nextHandleId
+    this.nextHandleId += 1
 
-    this.setUint32(handle_a_ptr, idA);
-    this.setUint32(handle_b_ptr, idB);
-    postMessage({msg: "kp_channel_create", a_id: idA, b_id: idB});
+    this.setUint32(handle_a_ptr, idA)
+    this.setUint32(handle_b_ptr, idB)
+    postMessage({ msg: 'kp_channel_create', a_id: idA, b_id: idB })
   }
 
-  kp_ring_create(handle_ptr, ring_params_ptr, buf_ptr, buf_len) {
-    const id = this.nextHandleId;
-    this.nextHandleId += 1;
+  kp_ring_create (handle_ptr, ring_params_ptr, buf_ptr, buf_len) {
+    const id = this.nextHandleId
+    this.nextHandleId += 1
 
-    let calc_len = (n) => {
-      let reqs = Math.pow(2, n);
-      let size = 7*4 + reqs*28 + reqs*2*16;
-      return size;
-    };
-    let n = 0;
-    while (calc_len(n+1) < buf_len) {
-      n += 1;
+    const calc_len = (n) => {
+      const reqs = Math.pow(2, n)
+      const size = 7 * 4 + reqs * 28 + reqs * 2 * 16
+      return size
+    }
+    let n = 0
+    while (calc_len(n + 1) < buf_len) {
+      n += 1
     }
     if (n === 0) {
       // buffer not long enough for even a tiny ring
-      console.warn("buffer was too small for even zero length ring");
-      return 1;
+      console.warn('buffer was too small for even zero length ring')
+      return 1
     }
 
-    let ptrs = {
+    const ptrs = {
       flags: 0 * 4 + buf_ptr,
 
-      request_head: 1*4 + buf_ptr,
-      request_tail: 2*4 + buf_ptr,
-      request_count: 3*4 + buf_ptr,
-      request_first: 7*4 + buf_ptr,
+      request_head: 1 * 4 + buf_ptr,
+      request_tail: 2 * 4 + buf_ptr,
+      request_count: 3 * 4 + buf_ptr,
+      request_first: 7 * 4 + buf_ptr,
 
-      response_head: 4*4 + buf_ptr,
-      response_tail: 5*4 + buf_ptr,
-      response_count: 6*4 + buf_ptr,
-      response_first: 7*4 + Math.pow(2, n)*28 + buf_ptr,
-    };
+      response_head: 4 * 4 + buf_ptr,
+      response_tail: 5 * 4 + buf_ptr,
+      response_count: 6 * 4 + buf_ptr,
+      response_first: 7 * 4 + Math.pow(2, n) * 28 + buf_ptr
+    }
 
-    this.setUint32(ptrs.flags, 0);
-    this.setUint32(ptrs.request_head, 0);
-    this.setUint32(ptrs.request_tail, 0);
-    this.setUint32(ptrs.request_count, 0);
-    this.setUint32(ptrs.response_head, 0);
-    this.setUint32(ptrs.response_tail, 0);
-    this.setUint32(ptrs.response_count, 0);
+    this.setUint32(ptrs.flags, 0)
+    this.setUint32(ptrs.request_head, 0)
+    this.setUint32(ptrs.request_tail, 0)
+    this.setUint32(ptrs.request_count, 0)
+    this.setUint32(ptrs.response_head, 0)
+    this.setUint32(ptrs.response_tail, 0)
+    this.setUint32(ptrs.response_count, 0)
 
-    this.setUint32(ring_params_ptr + 0*4, ptrs.flags);
-    this.setUint32(ring_params_ptr + 1*4, ptrs.request_head);
-    this.setUint32(ring_params_ptr + 2*4, ptrs.request_tail);
-    this.setUint32(ring_params_ptr + 3*4, ptrs.request_count);
-    this.setUint32(ring_params_ptr + 4*4, ptrs.response_head);
-    this.setUint32(ring_params_ptr + 5*4, ptrs.response_tail);
-    this.setUint32(ring_params_ptr + 6*4, ptrs.response_count);
+    this.setUint32(ring_params_ptr + 0 * 4, ptrs.flags)
+    this.setUint32(ring_params_ptr + 1 * 4, ptrs.request_head)
+    this.setUint32(ring_params_ptr + 2 * 4, ptrs.request_tail)
+    this.setUint32(ring_params_ptr + 3 * 4, ptrs.request_count)
+    this.setUint32(ring_params_ptr + 4 * 4, ptrs.response_head)
+    this.setUint32(ring_params_ptr + 5 * 4, ptrs.response_tail)
+    this.setUint32(ring_params_ptr + 6 * 4, ptrs.response_count)
 
-    postMessage({msg: "kp_ring_create", ring_id: id, ptrs});
+    postMessage({ msg: 'kp_ring_create', ring_id: id, ptrs })
 
-    this.setUint32(handle_ptr, id);
-    this.rings[id] = ptrs;
+    this.setUint32(handle_ptr, id)
+    this.rings[id] = ptrs
 
-    return 0;
+    return 0
   }
 
-  completedItemsInRing(ring) {
-    return roundUint32(this.getUint32(ring.response_tail, true) - this.getUint32(ring.response_head, true));
+  completedItemsInRing (ring) {
+    return roundUint32(this.getUint32(ring.response_tail, true) - this.getUint32(ring.response_head, true))
   }
 
-  kp_ring_enter(ringId, min_complete, max_time) {
+  kp_ring_enter (ringId, min_complete, max_time) {
     if (max_time === 0) {
-      max_time = Infinity;
+      max_time = Infinity
     }
-    let ring = this.rings[ringId];
+    const ring = this.rings[ringId]
     if (!ring) {
-      console.error("unknown ring:", ring);
-      return 1;
+      console.error('unknown ring:', ring)
+      return 1
     }
-    let flags = this.getUint32(ring.flags, true);
+    const flags = this.getUint32(ring.flags, true)
     if ((flags & 0b10) === 0) {
-      postMessage({msg: "kp_ring_enter", ring_id: ringId});
+      postMessage({ msg: 'kp_ring_enter', ring_id: ringId })
     }
 
-    let tail = this.getUint32(ring.response_tail, true);
-    let head = this.getUint32(ring.response_head, true);
+    let tail = this.getUint32(ring.response_tail, true)
+    let head = this.getUint32(ring.response_head, true)
 
-    while (roundUint32(tail-head) < min_complete) {
-      console.info("THREAD WAITING ON", ring.response_tail)
+    while (roundUint32(tail - head) < min_complete) {
+      console.info('THREAD WAITING ON', ring.response_tail)
       // TODO if this loops around because a flag was changed, we want to subtract the elapsed time from max_time
-      if (this.wait(ring.response_tail, tail, max_time) === "timed-out") {
-        break;
+      if (this.wait(ring.response_tail, tail, max_time) === 'timed-out') {
+        break
       }
-      tail = this.getUint32(ring.response_tail, true);
-      head = this.getUint32(ring.response_head, true);
+      tail = this.getUint32(ring.response_tail, true)
+      head = this.getUint32(ring.response_head, true)
     }
 
-    return 0;
+    return 0
   }
 
-  kp_generic_close(handle) {
+  kp_generic_close (handle) {
     if (this.rings[handle]) {
-      this.rings[handle] = false;
+      this.rings[handle] = false
     } else if (this.channels[handle]) {
       // TODO HANDLE CLOSE FROM OTHER SIDE
-      this.channels[handle] = false;
+      this.channels[handle] = false
     } else {
-      console.error("attempted to close unknown handle:", handle);
+      console.error('attempted to close unknown handle:', handle)
       return
     }
-    postMessage({msg: "kp_generic_close", handle: id});
+    postMessage({ msg: 'kp_generic_close', handle: id })
   }
 
   // TODO fork should be at a higher level (like at process creation) than syscalls i think
-  kp_fork(us) {
+  kp_fork (us) {
     this.wrap_async(done => {
-      console.error("call to unimplemented function kp_fork")
+      console.error('call to unimplemented function kp_fork')
       setTimeout(done, 1000)
     })
   }
 
-  kp_debug_msg(num) {
+  kp_debug_msg (num) {
     console.log(num)
   }
 
-  kp_bootstrap(handle_ptr) {
-    console.error("call to unimplemented function kp_args")
+  kp_bootstrap (handle_ptr) {
+    console.error('call to unimplemented function kp_args')
   }
 
-  kp_atomic_store_u32(ptr, val) {
+  kp_atomic_store_u32 (ptr, val) {
     Atomics.store(this.memoryViewU32, ptr >> 2, val)
   }
 
-  kp_atomic_load_u32(ptr) {
+  kp_atomic_load_u32 (ptr) {
     return Atomics.load(this.memoryViewU32, ptr >> 2)
   }
 
-  async run_main(memory) {
-    let env = {};
-    env.memory = memory;
+  async run_main (memory) {
+    const env = {}
+    env.memory = memory
     this.memory = memory;
     [
-      "kp_channel_create",
-      "kp_ring_create",
-      "kp_ring_enter",
-      "kp_generic_close",
-      "kp_fork",
-      "kp_debug_msg",
-      "kp_bootstrap",
-      "kp_atomic_store_u32",
-      "kp_atomic_load_u32",
-    ].forEach(fn => {env[fn] = (...args) => this[fn](...args)});
-    let results = await WebAssembly.instantiate(this.wasmBytes, {env});
-    this.instance = results.instance;
-    this.rewindBufferPtr = this.instance.exports.stack_buffer_alloc(1024 + 8);
-    this.memoryView = new Uint8Array(this.memory.buffer);
-    this.memoryViewU32 = new Uint32Array(this.memory.buffer);
-    this.memoryViewI32 = new Int32Array(this.memory.buffer);
-    this.instance.exports.main();
-    this.instance.exports.asyncify_stop_unwind();
+      'kp_channel_create',
+      'kp_ring_create',
+      'kp_ring_enter',
+      'kp_generic_close',
+      'kp_fork',
+      'kp_debug_msg',
+      'kp_bootstrap',
+      'kp_atomic_store_u32',
+      'kp_atomic_load_u32'
+    ].forEach(fn => { env[fn] = (...args) => this[fn](...args) })
+    const results = await WebAssembly.instantiate(this.wasmBytes, { env })
+    this.instance = results.instance
+    this.rewindBufferPtr = this.instance.exports.stack_buffer_alloc(1024 + 8)
+    this.memoryView = new Uint8Array(this.memory.buffer)
+    this.memoryViewU32 = new Uint32Array(this.memory.buffer)
+    this.memoryViewI32 = new Int32Array(this.memory.buffer)
+    this.instance.exports.main()
+    this.instance.exports.asyncify_stop_unwind()
   }
 }
 
 const mod = (x, n) => (x % n + n) % n
 
-function roundUint32(n) {
-  return mod(n, Math.pow(2, 32));
+function roundUint32 (n) {
+  return mod(n, Math.pow(2, 32))
 }
 
-let started = false;
-onmessage = function(e) {
+let started = false
+onmessage = function (e) {
   if (started) {
-    return;
+    return
   }
-  started = true;
+  started = true
 
   fetch(e.data.wasmFile).then(response =>
     response.arrayBuffer()
   ).then(bytes => {
-    let instance = new Instance(bytes);
-    instance.run_main(e.data.memory).catch(console.error);
+    const instance = new Instance(bytes)
+    instance.run_main(e.data.memory).catch(console.error)
   })
 }
